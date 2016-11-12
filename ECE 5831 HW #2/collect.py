4991e087bd2b768887a38fa83386dc57dd28bc2e
Clayton Northey <claytonnorthey@Claytons-MacBook-Pro.local>
2016-11-12 14:10:02 -0500

    getting distance matrices

--- ECE 5831 HW #2/collect.py
+++ ECE 5831 HW #2/collect.py
@@ -58,13 +58,17 @@ def get_avg_array(num_images):
 			avg_face[r_index][c_index] = sum(f[r_index][c_index] for f in num_images)/list_len
 	return avg_face
 
-
-
-
+def get_distance_per_average(total_avg, face_avg):
+	distance_face = [[0.0]*DIMENSIONS[1]]*DIMENSIONS[0]
+	for r_index, row in enumerate(distance_face):
+		for c_index, column in enumerate(row):
+			distance_face[r_index][c_index] = face_avg[r_index][c_index] - total_avg[r_index][c_index]
+	return distance_face
 
 if __name__=='__main__':
 	faces = {}
 	avg_faces = {}
+	distance_faces = {}
 	for dir_i in range(1, 41):
 		faces_tmp = []
 		for file_i in range(1, 11):
@@ -76,8 +80,9 @@ if __name__=='__main__':
 		faces[dir_i] = copy.copy(faces_tmp)
 		avg_faces[dir_i] = get_average_face(faces[dir_i])
 	tot_avg = get_avg_array([avg_faces[i] for i in range(1,11)])
-
-	print(len(tot_avg), len(tot_avg[0]))
+	for i in range(1,41):
+		distance_faces[i] = get_distance_per_average(tot_avg, avg_faces[i])
+	print(distance_faces[1][0])
 
 
 
