bc0c19e33504b522c466e81b7442738d99f44a9a
Clayton Northey <claytonnorthey@Claytons-MacBook-Pro.local>
2016-11-12 10:47:42 -0500

    getting average faces successfully

--- ECE 5831 HW #2/collect.py
+++ ECE 5831 HW #2/collect.py
@@ -4,7 +4,7 @@
 import copy
 
 FACES_DIR = 'orl_faces'
-
+META_BYTES = 14
 # takes in the conent of a file and returns information about the 
 # file according to PGM spec
 
@@ -16,15 +16,12 @@ class Face:
 	parsed_content = None
 
 	def __init__(self, content):
-		parts = content.split('\n')
-		self.magic_number = parts[0]
-		self.dimensions = parts[1].split(' ')
-		self.max_val = parts[2]
-		self.content = parts[3]
+		self.dimensions = [92, 112]
+		self.content = content[META_BYTES:]
 		self._set_content()
 
 	def get_dimensions(self):
-		return [int(d) for d in self.dimensions]
+		return self.dimensions
 
 	def _set_content(self):
 		columns = self.get_dimensions()[1]
@@ -38,8 +35,20 @@ class Face:
 	def get_content(self):
 		return self.parsed_content
 
+# gets the average face give a list of faces
+def get_average_face(faces):
+	num_faces = float(len(faces))
+	face_content = [f.get_content() for f in faces]
+	dimensions = faces[0].get_dimensions()
+	avg_face = [[0.0]*dimensions[1]]*dimensions[0]
+	for r_index, row in enumerate(avg_face):
+		for c_index, column in enumerate(row):
+			avg_face[r_index][c_index] = sum(f[r_index][c_index] for f in face_content)/num_faces
+	return avg_face
+
 if __name__=='__main__':
 	faces = {}
+	avg_faces = {}
 	for dir_i in range(1, 41):
 		faces_tmp = []
 		for file_i in range(1, 11):
@@ -49,4 +58,4 @@ if __name__=='__main__':
 			this_face = Face(file_content)
 			faces_tmp.append(this_face)
 		faces[dir_i] = copy.copy(faces_tmp)
-	print(faces)
+		avg_faces[dir_i] = get_average_face(faces[dir_i])
