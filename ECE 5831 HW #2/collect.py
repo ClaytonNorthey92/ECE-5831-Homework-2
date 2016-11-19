a508048028ee5e06d02359499ca3d1ff390959a7
Clayton Northey <claytonnorthey@Claytons-MacBook-Pro.local>
2016-11-19 14:19:25 -0500

    working

--- ECE 5831 HW #2/collect.py
+++ ECE 5831 HW #2/collect.py
@@ -59,9 +59,33 @@ def get_covariance(faces, avg_face):
 	covs = [numpy.cov(face) for face in faces]
 	return sum(covs)/len(faces)
 
-def project_face():
-	#to-do
-	pass
+def reconstruct_face(top_eigein_vectors, distance_faces):
+	new_face = [[None]*len(distance_faces) for x in range(len(top_eigein_vectors))]
+	for i in range(len(distance_faces)):
+		for j in range(len(top_eigein_vectors)):
+			new_face[j][i] = top_eigein_vectors[j].transpose()*distance_faces[i]
+
+	return new_face
+
+
+def get_some_original_face(total_avg):
+	faces = []
+	for i in range(1,41):
+		for j in range(1,11):
+			f = open('{}/s{}/{}.pgm'.format(FACES_DIR, i, j), 'rb')
+			file_content = f.read()
+			f.close()
+			this_face = Face(file_content)
+			distance_face = get_distance_per_average(total_avg, this_face.get_content())
+			face_covariance = get_covariance([distance_face], total_avg)
+			eigen_values, eigen_vectors = numpy.linalg.eig(face_covariance)
+			top_eigein_vectors[:40]
+			new_face = reconstruct_face(top_eigein_vectors, [distance_face])
+			faces.append(new_face)
+	return faces
+# To find corresponding face, funnel the error distances into a 1x40 array
+# Do a minnimum of the the array, return min index
+
 
 if __name__=='__main__':
 	faces = {}
@@ -85,4 +109,39 @@ if __name__=='__main__':
 	faces_covariance = get_covariance(distance_faces, total_avg)
 	eigen_values, eigen_vectors = numpy.linalg.eig(faces_covariance)
 
-	project_face()
+	top_eigein_vectors = eigen_vectors[:40]
+
+	new_face = reconstruct_face(top_eigein_vectors, distance_faces)
+	correct_faces = 0
+	incorrect_faces = 0
+	stored_faces = []
+	for i in range(40):
+		stored_faces.append([new_face[j][i] for j in range(40)])
+	classifed = {}
+	for f_index, face in enumerate(get_some_original_face(total_avg)):
+		current_face = f_index / 10
+		this_face = [element[0] for element in face]
+		errors = {}
+		for i in range(len(stored_faces)):
+			diff = abs(numpy.array(this_face) - numpy.array(stored_faces[i]))
+			if i not in errors:
+				errors[i] = 0
+			errors[i] += numpy.sum(diff)
+		min_error = None
+		min_key = None
+		for key in errors:
+			if not min_error or errors[key] < min_error:
+				min_error = errors[key]
+				min_key = key
+		classifed[f_index] = min_key
+	correct = 0
+	incorrect = 0
+	for key in classifed:
+		expected = key/10
+		if expected == classifed[key]:
+			correct += 1
+		else:
+			incorrect += 1
+		print "Classified {} as {}, expected {}".format(key, classifed[key], expected)
+	print "{} correct, {} incorrect".format(correct, incorrect)
+
